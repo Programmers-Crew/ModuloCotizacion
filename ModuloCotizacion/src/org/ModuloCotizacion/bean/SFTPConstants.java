c1fb837720d01efe1373c0592a2e5f19ec58a76d
Davis <davisrdln@gmail.com>
2021-08-30 14:03:14 -0600

    se subió imagen al servidor

---
+++ ModuloCotizacion/src/org/ModuloCotizacion/bean/SFTPConstants.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ModuloCotizacion.bean;
+
+/**
+ *
+ * @author Davis
+ */
+public class SFTPConstants {
+     public static final String SFTP_REQ_HOST = "alcongt.com";
+    public static final String SFTP_REQ_PORT = "22";
+    public static final String SFTP_REQ_USERNAME = "alcongt";
+    public static final String SFTP_REQ_PASSWORD = "Z3UxyCb3-2021";
+    public static final int SFTP_DEFAULT_PORT = 22;
+    public static final String SFTP_REQ_LOC = "location";
+}
